1f983f9d98d8d30652816e57210987ceef6ab617
Prakhar Pratap Singh <prakhar0401@gmail.com>
2019-04-05 23:13:25 +0530

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 const http=require('http');
+const path=require('path');
+const fs=require('fs');
 var port=3000;
 var hostName ='localhost';
 
 
 var server=http.createServer(function(req,res){
-    console.log(req.headers);
-res.statusCode=200;
-                     //what will res.body contain
-res.end(`hello world ${res.url}`);
+    console.log("request by"+req.url+"by method"+req.method);
+if(req.method=='GET')
+{
+    var fileUrl;
+    if(req.url=='/')
+        {
+            fileUrl='/a.html';
+        }
+    else
+            fileUrl=req.url;
+    var filePath=path.resolve('./public'+fileUrl);
+    const fileExt=path.extname(filePath);
+    if(fileExt=='.html')
+    {
+        fs.exists(filePath,function(exists){
+            if(!exists)
+            {res.statusCode=404;
+            res.setHeader('Context-Type','html/text');
+            res.end('<html><body><h1>this file does not exist</h1></body></html>');
+            return;
+            }
+            res.statusCode=200;
+            res.setHeader('Context-Type','html/text');
+            fs.createReadStream(filePath).pipe(res);
+
+        })
+    }
+    else
+    {res.statusCode=404;
+        res.setHeader('Context-Type','html/text');
+        res.end('<html><body><h1>this file is not a html file</h1></body></html>');
+    return;
+    }
+}
+else
+{}
+
 });
+
+
+
+
+
 
 server.listen(port,hostName,function(){
     console.log(`server is running at http://${hostName}:${port}`);
